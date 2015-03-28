1b114622061287e4fdc9f71169f7adb38c982332
31212591 <31212591@099056-P31S204.macklabs.br>
2015-03-27 21:22:14 -0300

    Primeiro commit do projeto IMC

---
+++ IMC_31212591/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author 31212591
+ */
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ex2 ex = new Ex2();
+        ex.setLocation(400, 200);
+        ex.setVisible(true);
+    }
+}
